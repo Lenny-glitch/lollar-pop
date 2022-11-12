d3710e52272d27edd908378a81bc17d102f0d2bc
unknown <jonathanlollar@gmail.com>
2022-11-12 14:39:19 -0800

    jon test

---
+++ my-app/src/components/MapTools/Posts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {
+    // rest of the elements/components imported remain same
+    Outlet,
+  } from "react-router-dom";
+
+export default function Posts() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>Blog</h2>
+        {/* render any matching child */}
+        <Outlet />
+      </div>
+    );
+  }
+  
